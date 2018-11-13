5956fc8f3da6266db3f4896bb20923c3b851a2b7
Wojciech Muła <wojciech_mula@poczta.onet.pl>
2018-11-13 21:18:29 +0100

    A bit smarter sse also generation
    The instruction names might have different prefixes or suffixes,
    but they basically do the same (like padd and vpadd).

--- generate.py
+++ generate.py
@@ -6,6 +6,9 @@ from os.path import islink
 import os
 import codecs
 
+class Struct(object):
+    pass
+
 
 class Generator(object):
     def __init__(self, date, version, entries):
@@ -121,17 +124,44 @@ class Generator(object):
 
 
     def see_also(self, entry):
-        list = []
-        for instr, _ in entry.instructions:
-            try:
-                list.extend(self.by_instruction[instr])
-            except KeyError:
-                break
+        s = Struct()
+        s.list = []
 
-        if len(list) <= 1:
+        def get_entries(instruction):
+            try:
+                s.list.extend(self.by_instruction[instruction])
+            except KeyError:
+                pass
+
+        for instr, _ in entry.instructions:
+            get_entries(instr)
+
+            if instr.startswith('p') or instr.startswith('vp'):
+                # integer instructions
+
+                get_entries('vp' + instr[1:]) # try match pshufb [SSE] with vpshufb [AVX]
+                get_entries('v'  + instr[2:]) # or vice-versa
+
+            elif instr.endswith('ps') or instr.endswith('pd') or instr.endswith('ss'):
+                # floating point instructions
+                get_entries(instr[:-2] + 'pd') # addXX -> addpd
+                get_entries(instr[:-2] + 'ps') # addXX -> addpd
+                get_entries(instr[:-2] + 'ss') # addXX -> addss
+
+                if instr.startswith('v'):
+                    get_entries(instr[1:-2] + 'pd') # vaddXX -> addpd
+                    get_entries(instr[1:-2] + 'ps') # vaddXX -> addpd
+                    get_entries(instr[1:-2] + 'ss') # vaddXX -> addss
+                else:
+                    get_entries('v' + instr[:-2] + 'pd') # addXX -> vaddpd
+                    get_entries('v' + instr[:-2] + 'ps') # addXX -> vaddpd
+                    get_entries('v' + instr[:-2] + 'ss') # addXX -> vaddss
+
+
+        if len(s.list) <= 1:
             return
 
-        list = sorted(set(list))
+        list = sorted(set(s.list))
 
         return [item.name for item in list if item.name != entry.name]
 
