296718e2391e0ef61d0198b3694537dc6a26d9da
Wojciech Muła <wojciech_mula@poczta.onet.pl>
2018-11-14 20:09:31 +0100

    Preliminary support for uops.info
    Added loader that handles properly the current version of db

---
+++ lib/uops/loader.py
@@ -0,0 +1,127 @@
+import xml.etree.ElementTree as ET
+import sys
+
+from model import *
+
+def load(path, verbose=True):
+    log("Loading data from '%s'..." % path)
+    data = ET.parse(path)
+
+    log("Processing instructions...")
+    instructions = []
+    for instruction in data.getroot().getiterator('instruction'):
+        name = instruction.attrib['string']
+        log("loading %s" % name)
+        instr = parse_instruction(instruction)
+        if len(instr.measurements):
+            instructions.append(instr)
+        else:
+            log("no measurements for %s" % name)
+
+
+def parse_instruction(instruction):
+    data = Instruction()
+    data.name = instruction.attrib['asm'].lower()
+    data.form = instruction.attrib['string'].lower()
+
+    tmp = {}
+    for architecture in instruction.getiterator('architecture'):
+        name = architecture.attrib['name']
+        measurements = parse_measurements(architecture)
+        if measurements:
+            tmp[name] = measurements
+
+    data.measurements = tmp
+    print data
+
+    return data
+
+
+def parse_measurements(architecture):
+    result = []
+    # we favour real-world measurements over IACA results
+    for item in architecture.getiterator('measurement'):
+        result.append(parse_measurement(item))
+
+    if not result:
+        # if the worst cames to true, we pick the latest IACA
+        iaca = None
+        for item in architecture.getiterator('IACA'):
+            tmp = parse_iaca(item)
+            if iaca is None or tmp.version > iaca.version:
+                iaca = tmp
+
+        if iaca:
+            result = [iaca]
+
+    return result
+
+
+def parse_measurement(measurement):
+    data = Measurement()
+    data.throughput   = optional_float(measurement.attrib, 'throughput')
+    data.total_uops   = int(measurement.attrib['total_uops'])
+    data.uops_details = parse_ports(measurement)
+
+    # we're not dig into latency conditions, just record cycles
+    tmp = set()
+    for latency in measurement.getiterator('latency'):
+        try:
+            cycles = int(latency.attrib['cycles'])
+            tmp.add(cycles)
+        except KeyError:
+            min = int(latency.attrib['minCycles'])
+            max = int(latency.attrib['maxCycles'])
+            for cycles in range(min, max+1):
+                tmp.add(cycles)
+
+    if tmp:
+        data.latency = tuple(sorted(tmp))
+
+    return data
+
+
+def parse_iaca(iaca):
+    data = IACAMeasurements()
+    data.version      = float(iaca.attrib['version'])
+    data.latency      = optional_float(iaca.attrib, 'latency')
+    data.throughput   = optional_float(iaca.attrib, 'throughput')
+    data.total_uops   = int(iaca.attrib['total_uops'])
+    data.uops_details = parse_ports(iaca)
+
+    return data
+
+
+def parse_ports(tag):
+    l = []
+    for attr, value in tag.attrib.iteritems():
+        if not attr.startswith('port'):
+            continue
+
+        d = PortDetail()
+        d.ports = attr[4:]
+        d.uops  = int(value)
+        l.append(d)
+
+    return tuple(l)
+
+
+def optional_float(dict, key):
+    try:
+        return float(dict[key])
+    except KeyError:
+        pass
+
+
+def optional_int(dict, key):
+    try:
+        return int(dict[key])
+    except KeyError:
+        pass
+
+
+if __name__ == '__main__':
+    # XXX: for tests only
+    def log(s):
+        print s
+    load(sys.argv[1], log)
---
+++ lib/uops/model.py
@@ -0,0 +1,77 @@
+class Instruction(object):
+    __slots__ = (
+        'name',
+        'form',
+        'measurements',
+    )
+
+    def __init__(self):
+        self.name = None
+        self.form = None
+        self.measurements = []
+
+
+    def __len__(self):
+        return self.measurements
+
+
+    def __str__(self):
+        return '%s: %s' % (self.form, self.measurements)
+
+    __repr__ = __str__
+
+
+class Measurement(object):
+    __slots__ = (
+        'latency',
+        'throughput',
+        'total_uops',
+        'uops_details',
+    )
+
+    def __init__(self):
+        self.latency    = None
+        self.throughput = None
+        self.total_uops = None
+        self.uops_details = []
+
+
+    def __str__(self):
+        return '<Measurement: %s, %s, %s, %s>' % \
+               (self.latency, self.throughput, self.total_uops, self.uops_details)
+
+    __repr__ = __str__
+
+
+class PortDetail(object):
+    __slots__ = (
+        'ports',
+        'uops',
+    )
+
+    def __init__(self):
+        self.ports = None
+        self.uops  = None
+
+
+    def __str__(self):
+        return '<p%s: %d>' % (self.ports, self.uops)
+
+    __repr__ = __str__
+
+
+class IACAMeasurements(Measurement):
+    __slots__ = (
+        'latency',
+        'throughput',
+        'total_uops',
+        'uops_details',
+        'version'
+    )
+
+    def __init__(self):
+        super(Measurement, self).__init__()
+
+    def __str__(self):
+        return '<IACA %s: %s, %s, %s, %s>' % \
+        (self.version, self.latency, self.throughput, self.total_uops, self.uops_details)
